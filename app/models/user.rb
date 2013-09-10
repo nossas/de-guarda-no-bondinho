b1bc938cd9e9436cb05f79943dcccd931ca165aa
Nícolas Iensen <nicolas@iensen.me>
2013-09-10 11:40:10 -0300

    [#56628684] add validate format to User#email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   validates :name, :email, :phone, presence: true
+  validates :email, format: { with: /([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}/ }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,6 @@ describe User do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:phone) }
+  it { should allow_value("email@addresse.foo").for(:email) }
+  it { should_not allow_value("foo").for(:email) }
 end
