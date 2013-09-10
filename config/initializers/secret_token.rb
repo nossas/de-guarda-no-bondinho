5dc28920de3dd791515234177cea7efddceb6184
Nícolas Iensen <nicolas@iensen.me>
2013-09-10 06:31:32 -0300

    [#56628684] Rails 4 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeGuardaNoBondinho::Application.config.secret_key_base = 'eccab15bf381e1a8025878ea0a925f1171262195407fb4064e2be69cd28caab5dd2ba30ddb315685da4ad1d67b4a0529a9b53c912d47209dcbf7770593f08365'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeGuardaNoBondinho::Application.config.session_store :cookie_store, key: '_de-guarda-no-bondinho_session'
