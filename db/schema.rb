9a245b92ff5d687e55ce8655134bc46f3768e980
Nícolas Iensen <nicolas@iensen.me>
2013-09-11 12:39:03 -0300

    [#56628684] remove picture column from users; configure AWS on carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911144556) do
+ActiveRecord::Schema.define(version: 20130911152458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20130911144556) do
     t.string   "name"
     t.string   "email"
     t.string   "phone"
-    t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar"
